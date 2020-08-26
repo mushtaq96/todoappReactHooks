81a5ab8382a0a36342108672ddc1a1920b6c459f
mushtaq96 <mushtaq96smb@gmail.com>
2020-08-26 23:45:45 +0900

    can now add a to-do item to my list

--- src/App.js
+++ src/App.js
@@ -2,28 +2,58 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const [todos, setTodos]=useState([
-    {text:"Learn about react"},
-    {text:"Work from 9 am"},
-    {text:"Build people app"}
-  ])
+const Todo =({ todo })=><div className="todo">{todo.text}</div>
 
-  const Todo = ({ todo }) => <div className='todo'>{todo.text}</div>;
+function TodoForm({ addToDo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value)
+      return;
+    addToDo(value);
+    setValue("");
+  };
+
   return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e=>setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function App(){
+  const [todos, setTodos] = useState([
+    { text:"Learn about react" },
+    {text: "work rom 9 am"},
+    {text:"develop people react app"}
+  ]);
+
+  const addToDo = text=>{
+    const newTodos = [...todos,{ text }];
+    setTodos(newTodos);
+  };
+  return(
     <div className="app">
       <div className="todo-list">
+      
+      <TodoForm addToDo={addToDo}/>
+
         {todos.map((todo,index)=>(
-          <Todo 
+          <Todo
             key={index}
             index={index}
             todo={todo}
           />
-
         ))}
       </div>
     </div>
-  )
-}
 
+  );
+}
 export default App;
